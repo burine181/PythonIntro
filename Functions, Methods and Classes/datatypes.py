79eec329983fa77450fa05c8b48f433609c37fb2
burine181 <don.souders@gmail.com>
2026-06-10 16:53:36 -0700

    Initial Commit

---
+++ Functions, Methods and Classes/datatypes.py
@@ -0,0 +1,11 @@
+from functs.my_module import cube_number, greet_person
+from classes.my_classes import Car
+
+print(cube_number(3))
+print(greet_person("John"))
+
+my_car = Car(make="Ford", model="Expedition", year=2017, milage=125000, color="Black", condition="used")
+my_wife_car = Car(make="Jeep", model="Grand Cherokee", year=2022, milage=47000, color="Maroon", condition="new")
+
+print(my_car.__dict__)
+print(my_wife_car.__dict__)
